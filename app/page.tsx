9b6f8cb403a1e5868f0053f5442924180f0fc3ac
ecmcode <ecmcode.dev@gmail.com>
2023-07-31 14:58:27 -0500

    prettier: update 8 files

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 const HomePage = () => {
-  return (<div>
-    <h1>Landing Page Example</h1>
-  </div>);
-}
- 
+  return (
+    <div>
+      <h1>Landing Page Example</h1>
+    </div>
+  );
+};
+
 export default HomePage;
